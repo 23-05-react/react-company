ab1bd791e047f3652a3038c479b9b6fd07209be3
tubi55 <tubi55@nate.com>
2023-06-27 13:07:39 +0900

    유튜브 데이터 fetching후 렌더링

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,34 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
-	return <Layout name={'Youtube'}></Layout>;
+	const [Vids, setVids] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyCF8SOz4Cchg53VOMXZe0un2AC7zEP2apU';
+		const list = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+		const num = 10;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+		axios.get(url).then((data) => {
+			console.log(data.data.items);
+			setVids(data.data.items);
+		});
+	}, []);
+	return (
+		<Layout name={'Youtube'}>
+			{Vids.map((vid, idx) => {
+				return (
+					<article key={idx}>
+						<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+						<h2>{vid.snippet.title}</h2>
+						<p>{vid.snippet.description}</p>
+						<span>{vid.snippet.publishedAt}</span>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Youtube;
