b05bed953186d32e84a36dfbb3d34e4ee3c25bc7
tubi55 <tubi55@nate.com>
2023-06-28 09:35:00 +0900

    기본 유튜브 스타일링

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -15,15 +15,20 @@ function Youtube() {
 			setVids(data.data.items);
 		});
 	}, []);
+
 	return (
 		<Layout name={'Youtube'}>
 			{Vids.map((vid, idx) => {
 				return (
 					<article key={idx}>
-						<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
 						<h2>{vid.snippet.title.length > 50 ? vid.snippet.title.substr(0, 50) + '...' : vid.snippet.title}</h2>
-						<p>{vid.snippet.description.length > 200 ? vid.snippet.description.substr(0, 200) + '...' : vid.snippet.description}</p>
-						<span>{vid.snippet.publishedAt.split('T')[0].split('-').join('.')}</span>
+						<div className='txt'>
+							<p>{vid.snippet.description.length > 200 ? vid.snippet.description.substr(0, 200) + '...' : vid.snippet.description}</p>
+							<span>{vid.snippet.publishedAt.split('T')[0].split('-').join('.')}</span>
+						</div>
+						<div className='pic'>
+							<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+						</div>
 					</article>
 				);
 			})}
