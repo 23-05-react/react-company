68c53dca57912db032bed9087cf179f0b024e753
tubi55 <tubi55@nate.com>
2023-06-28 11:45:43 +0900

    컴포넌트 라우터 연결구문 축약

--- src/App.js
+++ src/App.js
@@ -20,42 +20,17 @@ import Main from './components/main/Main';
 function App() {
 	return (
 		<>
-			{/* Switch는 내부에 중복되는 라우트 경로가 있을때 먼저 나오는 라우터를 채택하고 나머지는 무시 */}
 			<Switch>
-				<Route exact path='/'>
-					<Main />
-				</Route>
-
-				<Route path='/'>
-					{/* 서브전용 라우터에는 sub문자값을 전달 */}
-					<Header type={'sub'} />
-				</Route>
+				<Route exact path='/' component={Main} />
+				<Route path='/' render={() => <Header type={'sub'} />} />
 			</Switch>
 
-			<Route path='/department'>
-				<Department />
-			</Route>
-
-			<Route path='/community'>
-				<Community />
-			</Route>
-
-			<Route path='/gallery'>
-				<Gallery />
-			</Route>
-
-			<Route path='/youtube'>
-				<Youtube />
-			</Route>
-
-			<Route path='/contact'>
-				<Contact />
-			</Route>
-
-			<Route path='/member'>
-				<Member />
-			</Route>
-
+			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/contact' component={Contact} />
+			<Route path='/member' component={Member} />
 			<Footer />
 		</>
 	);
