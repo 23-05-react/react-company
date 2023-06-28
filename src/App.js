54704e61bd3ff1c51312526016eab97fad718ea4
tubi55 <tubi55@nate.com>
2023-06-28 11:40:27 +0900

    메인전용 컴포넌트들을 Main컴포넌트로 패키징

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Footer from './components/common/Footer';
 import Header from './components/common/Header';
 
 //main
-import Visual from './components/main/Visual';
-import News from './components/main/News';
-import Pics from './components/main/Pics';
-import Vids from './components/main/Vids';
-import Banner from './components/main/Banner';
 
 //sub
 import Community from './components/sub/Community';
@@ -20,6 +15,7 @@ import Member from './components/sub/Member';
 import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
+import Main from './components/main/Main';
 
 function App() {
 	return (
@@ -27,13 +23,7 @@ function App() {
 			{/* Switch는 내부에 중복되는 라우트 경로가 있을때 먼저 나오는 라우터를 채택하고 나머지는 무시 */}
 			<Switch>
 				<Route exact path='/'>
-					{/* 메인전용 라우터에는 main문자값을 전달 */}
-					<Header type={'main'} />
-					<Visual />
-					<News />
-					<Pics />
-					<Vids />
-					<Banner />
+					<Main />
 				</Route>
 
 				<Route path='/'>
