f5ec5621b8ac1d498524b2f73f082c4b7c5d46f8
tubi55 <tubi55@nate.com>
2023-07-03 10:48:57 +0900

    현재 입력하고 있는 input항목의 name, value값을 실시간으로 기존 State객체에 덮어쓰기

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react';
 import Layout from '../common/Layout';
 
 function Member() {
+	const initVal = {
+		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+	};
+
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		//현재 입력하고 있는 input요소의 name,value값을 비구조화할당으로 뽑아서 출력
+		const { name, value } = e.target;
+		//기존 초기 Val State값을 deep copy해서 현재 입력하고 있는 항목의 name값과 value값으로 기존 State를 덮어쓰기 해서 변경 (불변성 유지)
+		setVal({ ...Val, [name]: value });
+	};
+
+	useEffect(() => {
+		console.log(Val);
+	}, [Val]);
+
 	return (
 		<Layout name={'Member'}>
 			<form>
@@ -14,7 +35,13 @@ function Member() {
 									<label htmlFor='userid'>USER ID</label>
 								</th>
 								<td>
-									<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' />
+									<input
+										type='text'
+										name='userid'
+										id='userid'
+										placeholder='아이디를 입력하세요'
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
@@ -24,7 +51,13 @@ function Member() {
 									<label htmlFor='pwd1'>PASSWORD</label>
 								</th>
 								<td>
-									<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' />
+									<input
+										type='password'
+										name='pwd1'
+										id='pwd1'
+										placeholder='비밀번호를 입력하세요'
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
@@ -34,7 +67,13 @@ function Member() {
 									<label htmlFor='pwd2'>RE-PASSWORD</label>
 								</th>
 								<td>
-									<input type='password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' />
+									<input
+										type='password'
+										name='pwd2'
+										id='pwd2'
+										placeholder='비밀번호를 재입력하세요'
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
@@ -44,7 +83,13 @@ function Member() {
 									<label htmlFor='email'>E-MAIL</label>
 								</th>
 								<td>
-									<input type='text' name='email' id='email' placeholder='이메일주소를 입력하세요' />
+									<input
+										type='text'
+										name='email'
+										id='email'
+										placeholder='이메일주소를 입력하세요'
+										onChange={handleChange}
+									/>
 								</td>
 							</tr>
 
