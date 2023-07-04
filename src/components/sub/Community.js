5cd663046b8b30e4d44e2137277f821f582d1479
tubi55 <tubi55@nate.com>
2023-07-04 11:17:40 +0900

    수정버튼 클릭시 해당 포스트만 수정되도록 전용 property추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -25,6 +25,15 @@ function Community() {
 		setPosts(Posts.filter((_, idx) => idx !== delIndex));
 	};
 
+	const enableUpdate = (editIndex) => {
+		setPosts(
+			Posts.map((post, postIndex) => {
+				if (editIndex === postIndex) post.enableUpdate = true;
+				return post;
+			})
+		);
+	};
+
 	useEffect(() => {
 		console.log(Posts);
 	}, [Posts]);
@@ -53,7 +62,7 @@ function Community() {
 							</div>
 
 							<nav className='btnSet'>
-								<button>EDIT</button>
+								<button onClick={() => enableUpdate(idx)}>EDIT</button>
 								<button onClick={() => deletePost(idx)}>DELETE</button>
 							</nav>
 						</article>
