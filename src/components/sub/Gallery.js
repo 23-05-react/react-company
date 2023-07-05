be8770ea5a81191dfd12263c12e724242f21609a
tubi55 <tubi55@nate.com>
2023-07-05 12:17:08 +0900

    gkatndp opt추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -9,16 +9,28 @@ function Gallery() {
 		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 		const key = 'ae5dbef0587895ed38171fcda4afb648';
 		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 		const num = 20;
+		//const myId = '164021883@N04';
 		let url = '';
+
 		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		if (opt.type === 'search')
+			url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+		if (opt.type === 'user')
+			url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
 
 		const result = await axios.get(url);
-		console.log(result.data.photos.photo);
 		setItems(result.data.photos.photo);
 	};
 
-	useEffect(() => getFlickr({ type: 'interest' }), []);
+	//미션1 - 아래 호출문으로 풍경이미지 검색되도록 함수 코드 수정
+	//getFlickr({type: 'search', tags: 'landscape'})
+
+	//미션2 - 아래 호출문으로 내 계정의 이미지 갤러리 호출되도록
+	//getFlickr({type: 'user', user: '내아이디'})
+	useEffect(() => getFlickr({ type: 'user', user: '164021883@N04' }), []);
 
 	return (
 		<Layout name={'Gallery'}>
