8883365bfa446fdf237d8d9e9ca42ff379d6ea0f
tubi55 <tubi55@nate.com>
2023-07-05 11:39:59 +0900

    flickr 데이터 fetching후 state에 담기

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,6 +1,23 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function Gallery() {
+	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+	const key = 'ae5dbef0587895ed38171fcda4afb648';
+	const method_interest = 'flickr.interestingness.getList';
+	const num = 20;
+	const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+
+	const [Items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios.get(url).then((json) => {
+			console.log(json.data.photos.photo);
+			setItems(json.data.photos.photo);
+		});
+	}, []);
+
 	return <Layout name={'Gallery'}>Gallery</Layout>;
 }
 
