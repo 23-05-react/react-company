d00d061d74a822a5e6af91ccf04d121b240cf2c5
tubi55 <tubi55@nate.com>
2023-07-05 11:01:26 +0900

    메인 페이지 미리보기 출력 오류 해결

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,19 +2,9 @@ import Layout from '../common/Layout';
 import { useRef, useState, useEffect } from 'react';
 
 function Community() {
-	const dummy = [
-		{ title: 'Hello6', content: 'Here comes description in detail.' },
-		{ title: 'Hello5', content: 'Here comes description in detail.' },
-		{ title: 'Hello4', content: 'Here comes description in detail.' },
-		{ title: 'Hello3', content: 'Here comes description in detail.' },
-		{ title: 'Hello2', content: 'Here comes description in detail.' },
-		{ title: 'Hello1', content: 'Here comes description in detail.' },
-	];
-
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
-		if (data) return JSON.parse(data);
-		else return dummy;
+		return JSON.parse(data);
 	};
 
 	const input = useRef(null);
