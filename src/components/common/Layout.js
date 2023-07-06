ac48e9e3a208efd961a2864451efce7e4915f03f
tubi55 <tubi55@nate.com>
2023-07-06 09:53:07 +0900

    모든이미지 소스가 로딩완료되고 masonry가 UI배치를 해줄떄까지 로딩 이미지 출력

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -15,7 +15,7 @@ function Layout({ name, children, txt = 'Default' }) {
 			<div className='inner'>
 				<h1>{name}</h1>
 				<h2>
-					{/* 해당 구분자로 문자값을 배열로 분리하고 반복을 돌면서 b태그 추가: 이때 주의할점 : 반복도는 요소이므로 key속성을 적용하기 위해서는 React.Fragement형태로 wrapping처리 */}
+					{/* 해당 구분자로 문자값을 배열로 분리하고 반복을 돌면서 br태그 추가: 이때 주의할점 : 반복도는 요소이므로 key속성을 적용하기 위해서는 React.Fragement형태로 wrapping처리 */}
 					{txt.split('-').map((el, idx) => {
 						return (
 							<React.Fragment key={idx}>
