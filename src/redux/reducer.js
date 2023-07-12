301ff3895af51a96f1f58f231824f539969e0bb0
tubi55 <tubi55@nate.com>
2023-07-12 10:11:42 +0900

    유튜브 데이터 액션생성함수, 리듀서함수 추가

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -35,8 +35,6 @@ const initMember = {
 	],
 };
 
-//초기 데이터값을 state로 지정하고 추후 action객체가 넘어오면 action의 타입에 따라 해당 데이터를 변경해주는
-//변형자 함수 생성
 const memberReducer = (state = initMember, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
@@ -46,6 +44,14 @@ const memberReducer = (state = initMember, action) => {
 	}
 };
 
-//해당 변형자 함수가 반환하는 객체값을 하나의 객체로 합쳐서 외부로  export
-const reducers = combineReducers({ memberReducer });
+const youtubeReducer = (state = { youtuebe: [] }, action) => {
+	switch (action.type) {
+		case 'SET_YOUTUBE':
+			return { ...state, youtube: action.payload };
+		default:
+			return state;
+	}
+};
+
+const reducers = combineReducers({ memberReducer, youtubeReducer });
 export default reducers;
