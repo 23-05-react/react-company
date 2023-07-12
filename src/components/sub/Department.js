60f8fb11a31cbb3ef94b6c45567d165c4547a167
tubi55 <tubi55@nate.com>
2023-07-12 09:52:50 +0900

    자식 컴포넌트에서 전역 데이터 변경요청

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,13 +1,28 @@
 import Layout from '../common/Layout';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMembers } from '../../redux/action';
 
 function Department() {
 	const Members = useSelector((store) => store.memberReducer.members);
+	const dispatch = useDispatch();
 
 	return (
-		// props으로 자식요소에 줄바꿈될 텍스트를 전달하고 싶을떄
-		//줄바꿀될 부분에 구분자가 될 문자값을 넣어서 전달
 		<Layout name={'Department'} txt={'Hello-World'}>
+			<button
+				onClick={() => {
+					//버튼 클릭시 기존 State값을 Deep copy
+					const newMembers = [...Members];
+					//Deep copy된 참조형 자료 State정보값을 변경후
+					newMembers[0].name = 'Emma';
+					//action생성함수의 인수로 넣어 새로운 액션객체 생성
+					const newAction = setMembers(newMembers);
+					console.log(newAction);
+					//그렇게 만들어진 액션객체를 dispatch를 통해 리듀서에 전달
+					dispatch(newAction);
+				}}
+			>
+				멤버 데이터 변경
+			</button>
 			{Members.map((member, idx) => {
 				return (
 					<article key={idx}>
