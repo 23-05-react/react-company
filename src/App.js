b01d226ff7a0cfa10d9517f7e7f1c09d4aca7e42
tubi55 <tubi55@nate.com>
2023-07-13 11:42:52 +0900

    App컴포넌트 마운트시 dispatch로 youtube-start액션객체 전달

--- src/App.js
+++ src/App.js
@@ -18,10 +18,17 @@ import Member from './components/sub/Member';
 import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as types from './redux/actionType';
 
-//Menu컴포넌를 App에서 호출한뒤 토글 객체를 각각 메인, 서브 헤더로 전달해서 토글 메뉴 기능이 동작하도록 수정
 function App() {
+	const dispatch = useDispatch();
 	const menu = useRef(null);
+
+	useEffect(() => {
+		dispatch({ type: types.YOUTUBE.start });
+	}, [dispatch]);
 
 	return (
 		<>
