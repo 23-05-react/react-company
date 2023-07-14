9569347b1e79b4837a1639356bf4478381d318b0
tubi55 <tubi55@nate.com>
2023-07-14 13:21:17 +0900

    App에서 youbue데이터 액션객체 전달

--- src/App.js
+++ src/App.js
@@ -19,9 +19,17 @@ import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
 
-//Menu컴포넌를 App에서 호출한뒤 토글 객체를 각각 메인, 서브 헤더로 전달해서 토글 메뉴 기능이 동작하도록 수정
+import { fetchYoutube } from './redux/youtubeSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 function App() {
 	const menu = useRef(null);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchYoutube());
+	}, [dispatch]);
 
 	return (
 		<>
