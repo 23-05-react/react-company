14a8e5fa6e4c5cd2de750409a1777f3eeed1b7ce
tubi55 <tubi55@nate.com>
2023-07-17 11:09:09 +0900

    swiper panel 스타일링

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -8,7 +8,7 @@ function Vids() {
 	console.log(Vids);
 	return (
 		<section id='vids' className='myScroll'>
-			<Swiper>
+			<Swiper loop={true} spaceBetween={50}>
 				{Vids.map((vid, idx) => {
 					if (idx >= 5) return null;
 
@@ -18,6 +18,7 @@ function Vids() {
 								<div className='pic'>
 									<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
 								</div>
+								<h2>{vid.snippet.title}</h2>
 							</div>
 						</SwiperSlide>
 					);
