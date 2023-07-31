44677af982966a51ddcc6d26e98ba4b7554b9686
tubi55 <tubi55@nate.com>
2023-07-31 10:12:37 +0900

    department custom react-query적용

--- src/App.js
+++ src/App.js
@@ -18,19 +18,11 @@ import Youtube from './components/sub/Youtube';
 
 import './scss/style.scss';
 
-import { fetchDepartment } from './redux/departmentSlice';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
 	const queryClient = new QueryClient();
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(fetchDepartment());
-	}, [dispatch]);
 
 	return (
 		<QueryClientProvider client={queryClient}>
--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,23 +1,24 @@
 import Layout from '../common/Layout';
-import { useSelector } from 'react-redux';
+import { useDepartmentQuery } from '../../hooks/useDepartmentQuery';
 
 function Department() {
-	const Members = useSelector((store) => store.department.data);
+	const { data: Members, isSuccess } = useDepartmentQuery();
 
 	return (
 		<Layout name={'Department'} txt={'Hello-World'} bg={'Department.jpg'}>
-			{Members.map((member, idx) => {
-				return (
-					<article key={idx}>
-						<div className='pic'>
-							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-							<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-						</div>
-						<h2>{member.name}</h2>
-						<p>{member.position}</p>
-					</article>
-				);
-			})}
+			{isSuccess &&
+				Members.map((member, idx) => {
+					return (
+						<article key={idx}>
+							<div className='pic'>
+								<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+								<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</article>
+					);
+				})}
 		</Layout>
 	);
 }
